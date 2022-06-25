5b6cc5721ace7ffbb6642546c069cf32ad9d80b0
SreeragBhaskarK <sreeragvk020@gmail.com>
2022-06-25 22:14:15 +0530

    new

--- assets/js/jquery.tagcanvas.js
+++ assets/js/jquery.tagcanvas.js
@@ -767,11 +767,11 @@
     }
     function MouseOut(e) {
       var cv = e.target || e.fromElement.parentNode, tc = TagCanvas.tc[cv.id];
-      if(tc) {
+      /*if(tc) {
        tc.mx = tc.my = -1;
        tc.UnFreeze();
        tc.EndDrag();
-      }
+      }*/
     }
     function MouseMove(e) {
       var i, t = TagCanvas, tc, p, tg = EventToCanvasId(e);
